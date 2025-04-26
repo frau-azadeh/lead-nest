14725759ebc606621cec04fd394bfc49f3e11fc9
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-04-26 13:43:51 +0330

    add font vazir

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ function App() {
   return (
     <>
       <div></div>
-      <h1 className="text-red-500">Vite + React</h1>
+      <h1 className="text-red-500">سلام</h1>
       <div className="card"></div>
       <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
     </>
