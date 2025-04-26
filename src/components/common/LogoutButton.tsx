388efa2fdc30bab3637876a726f5a264763ca1b7
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-04-26 19:50:54 +0330

    update

---
+++ src/components/common/LogoutButton.tsx
@@ -0,0 +1,19 @@
+// src/components/common/LogoutButton.tsx
+
+import { useAppDispatch } from '../../store/hooks';
+import { logoutUser } from '../../features/auth/authSlice';
+import { Button } from '../../components/ui'; // اگر دکمه خودتو ساختی
+
+export default function LogoutButton() {
+  const dispatch = useAppDispatch();
+
+  const handleLogout = () => {
+    dispatch(logoutUser());
+  };
+
+  return (
+    <Button onClick={handleLogout}>
+      خروج
+    </Button>
+  );
+}
