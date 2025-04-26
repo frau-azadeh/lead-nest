a3f4cb751e05f8344b3d5e0d760546583de1a765
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-04-26 18:11:04 +0330

    update

--- src/main.tsx
+++ src/main.tsx
@@ -6,14 +6,26 @@ import './index.css';
 import { Provider } from 'react-redux';
 import { store, persistor } from './store/store';
 import { PersistGate } from 'redux-persist/integration/react';
+import { Toaster } from 'react-hot-toast'; 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
+        <Toaster
+          position="top-center"
+          toastOptions={{
+            style: {
+              fontFamily: 'inherit',
+              direction: 'rtl',
+              textAlign: 'right',
+            },
+          }}
+        />
         <App />
       </PersistGate>
     </Provider>
   </React.StrictMode>
 );
+
 
