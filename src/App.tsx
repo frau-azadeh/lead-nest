8f236e1ddef2a882ad565d2191fae81032efe4d8
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-04-26 13:29:42 +0330

    update prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,11 @@
-
 import './App.css';
 
 function App() {
-  
   return (
     <>
-      <div>
-       
-      </div>
+      <div></div>
       <h1 className="text-red-500">Vite + React</h1>
-      <div className="card">
-       
-       
-      </div>
+      <div className="card"></div>
       <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
     </>
   );
