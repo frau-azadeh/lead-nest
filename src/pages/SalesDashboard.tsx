166075be65f9add532f212e8e1619b78dad8a2e0
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-04-26 20:37:07 +0330

    add SalesDashboard

---
+++ src/pages/SalesDashboard.tsx
@@ -0,0 +1,11 @@
+// src/pages/SalesDashboard.tsx
+
+export default function SalesDashboard() {
+    return (
+      <div className="text-center py-10">
+        <h1 className="text-3xl font-bold mb-4">داشبورد فروش</h1>
+        <p>خوش آمدید به بخش فروش.</p>
+      </div>
+    );
+  }
+  
