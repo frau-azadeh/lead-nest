c68c12a82f3ba2717f89fbb919d1247e5a2317aa
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-04-30 15:37:50 +0330

    add Pagination

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -5,3 +5,4 @@ export { default as Input } from './Input';
 export { default as Modal } from './Modal';
 export {default as Select } from './Select';
 export {default as Table} from './Table';
+export {default as Pagination} from './Pagination';
