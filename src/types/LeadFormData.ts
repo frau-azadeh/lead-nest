9f2a7011ed480feffc52f8406d18685043cf6fb2
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-05-04 09:16:03 +0330

    update

---
+++ src/types/LeadFormData.ts
@@ -0,0 +1,9 @@
+// types/LeadFormData.ts
+export interface LeadFormData {
+    full_name: string;
+    email: string;
+    phone_number?: string;
+    company?: string;
+    status: string; // فرض: اجباری
+  }
+  
