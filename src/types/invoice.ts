b8f77da07ae20e20c3c5e7660e242ffcb152d679
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-05-04 16:25:03 +0330

    update

---
+++ src/types/invoice.ts
@@ -0,0 +1,18 @@
+export interface Invoice {
+    id: string;
+    lead_id: string;
+    type: string;
+    amount: number;
+    product_name?: string; // 👈 این خط رو اضافه کن
+    created_at: string;
+  }
+
+  // types/invoice.ts
+export interface InsertInvoice {
+    lead_id: string;
+    type: string;
+    amount: number;
+    product_name?: string;
+  }
+  
+  
